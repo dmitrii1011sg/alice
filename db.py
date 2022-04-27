45642f1c2765c915d753d8cff5196983424f556a
Ryzhik Daniel <67552741+ZinixZay@users.noreply.github.com>
2022-04-27 22:08:23 +0300

    С БД и комментариями

--- db.py
+++ db.py
@@ -10,18 +10,18 @@ db = firebase.database()
 data = db.child('poems').get().val()
 
 
-def send_conclusion(msg: str, f: bool):
+def send_conclusion(msg: str, f: bool):  # Вспомогательная функция (скип)
     if f:
         print(Back.GREEN, Fore.BLACK + msg)
     else:
         print(Back.RED, Fore.BLACK + msg)
 
 
-def db_push(current_data: dict):
+def db_push(current_data: dict):  # Вспомогательная функция (скип)
     db.child('poems').set(current_data)
 
 
-def db_add_poem(author: str, title: str, content: str):
+def db_add_poem(author: str, title: str, content: str):  # Добавление стиха в каталог к уже соществующему автору
     if author in data.keys():
         if title.lower().capitalize() in data[author].keys():
             message = f'Стихотворение "{title}" У "{author}" уже существует'
@@ -36,7 +36,7 @@ def db_add_poem(author: str, title: str, content: str):
         send_conclusion(message, False)
 
 
-def db_add_poet(author: str):
+def db_add_poet(author: str):  # Добавление автора
     if author.lower().capitalize() in data.keys():
         message = f'{author} уже есть в базе данных'
         send_conclusion(message, False)
@@ -47,7 +47,7 @@ def db_add_poet(author: str):
         send_conclusion(message, True)
 
 
-def db_get_poem(author: str, title: str) -> str:
+def db_get_poem(author: str, title: str) -> str:  # Возвращает стихотворение
     if author in data.keys():
         if title.lower().capitalize() in data[author].keys():
             message = data[author][title.lower().capitalize()]
@@ -60,7 +60,7 @@ def db_get_poem(author: str, title: str) -> str:
         send_conclusion(message, False)
 
 
-def db_get_poet(author: str) -> dict:
+def db_get_poet(author: str) -> dict:  # Возвращает все стихотворения автора
     if author.lower().capitalize() in data.keys():
         poems = dict()
         for key, value in data[author.lower().capitalize()].items():
@@ -73,7 +73,7 @@ def db_get_poet(author: str) -> dict:
         return {}
 
 
-def db_get_poets() -> list:
+def db_get_poets() -> list:  # Возвращает список всех авторов
     poets = list()
     for i in data.keys():
         if i == 'sys': continue
@@ -81,7 +81,7 @@ def db_get_poets() -> list:
     return poets
 
 
-def db_delete_poem(author: str, title: str):
+def db_delete_poem(author: str, title: str):  # Удаляет стихотворение из каталога автора
     if author in data.keys():
         if title.lower().capitalize() in data[author].keys():
             data[author][title.lower().capitalize()] = {}
@@ -96,7 +96,7 @@ def db_delete_poem(author: str, title: str):
         send_conclusion(message, False)
 
 
-def db_delete_poet(author: str):
+def db_delete_poet(author: str):  # Удаляет автора и все его стихи из базы данных
     if author.lower().capitalize() in data.keys():
         if input(Back.WHITE + f'Вы уверены что хотите удалить "{author}" и все его стихи? [Y/N]') == 'Y':
             db[author] = {}
