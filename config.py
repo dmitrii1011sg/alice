74c950bda4a63127c1e5230197a4c8b38bd266fe
Ryzhik Daniel <67552741+ZinixZay@users.noreply.github.com>
2022-04-27 21:59:44 +0300

    Добавлена работающая БД

---
+++ config.py
@@ -0,0 +1,10 @@
+firebaseConfig = {
+  'apiKey': "AIzaSyDdBsVrTr03cDepbhHDYGquaIIf6J_x3d4",
+  'authDomain': "alice-7f960.firebaseapp.com",
+  'databaseURL': "https://alice-7f960-default-rtdb.firebaseio.com",
+  'projectId': "alice-7f960",
+  'storageBucket': "alice-7f960.appspot.com",
+  'messagingSenderId': "654866140336",
+  'appId': "1:654866140336:web:26a76e50b769703fa23f75",
+  'measurementId': "G-DF86200KLD"
+};
---
+++ db.py
@@ -0,0 +1,114 @@
+import colorama
+import pyrebase
+from colorama import Back, Fore
+from config import *
+
+colorama.init()
+
+firebase = pyrebase.initialize_app(firebaseConfig)
+db = firebase.database()
+data = db.child('poems').get().val()
+
+
+def send_conclusion(msg: str, f: bool):
+    if f:
+        print(Back.GREEN, Fore.BLACK + msg)
+    else:
+        print(Back.RED, Fore.BLACK + msg)
+
+
+def db_push(current_data: dict):
+    db.child('poems').set(current_data)
+
+
+def db_add_poem(author: str, title: str, content: str):
+    if author in data.keys():
+        if title.lower().capitalize() in data[author].keys():
+            message = f'Стихотворение "{title}" У "{author}" уже существует'
+            send_conclusion(message, False)
+        else:
+            data[author][title.lower().capitalize()] = content
+            db_push(data)
+            message = f'Стихотворение "{title}" добавлено в каталог к "{author}"'
+            send_conclusion(message, True)
+    else:
+        message = f'Этого автора нет в базе'
+        send_conclusion(message, False)
+
+
+def db_add_poet(author: str):
+    if author.lower().capitalize() in data.keys():
+        message = f'{author} уже есть в базе данных'
+        send_conclusion(message, False)
+    else:
+        data[author.lower().capitalize()] = {'sys': 0}
+        db_push(data)
+        message = f'{author} успешно добавлен в базу данных'
+        send_conclusion(message, True)
+
+
+def db_get_poem(author: str, title: str) -> str:
+    if author in data.keys():
+        if title.lower().capitalize() in data[author].keys():
+            message = data[author][title.lower().capitalize()]
+            return Fore.YELLOW + message
+        else:
+            message = f'Стихотворения "{title}" у "{author}" не найдено'
+            send_conclusion(message, False)
+    else:
+        message = f'Этого автора нет в базе'
+        send_conclusion(message, False)
+
+
+def db_get_poet(author: str) -> dict:
+    if author.lower().capitalize() in data.keys():
+        poems = dict()
+        for key, value in data[author.lower().capitalize()].items():
+            if key == 'sys': continue
+            poems[key] = value
+        return poems
+    else:
+        message = f'Этого автора нет в базе'
+        send_conclusion(message, False)
+        return {}
+
+
+def db_get_poets() -> list:
+    poets = list()
+    for i in data.keys():
+        if i == 'sys': continue
+        poets.append(i)
+    return poets
+
+
+def db_delete_poem(author: str, title: str):
+    if author in data.keys():
+        if title.lower().capitalize() in data[author].keys():
+            data[author][title.lower().capitalize()] = {}
+            db_push(data)
+            message = f'Стихотворение "{title}" удалено у "{author}"'
+            send_conclusion(message, True)
+        else:
+            message = f'Стихотворения "{title}" у "{author}" не найдено'
+            send_conclusion(message, False)
+    else:
+        message = f'Этого автора нет в базе.'
+        send_conclusion(message, False)
+
+
+def db_delete_poet(author: str):
+    if author.lower().capitalize() in data.keys():
+        if input(Back.WHITE + f'Вы уверены что хотите удалить "{author}" и все его стихи? [Y/N]') == 'Y':
+            db[author] = {}
+            db_push(data)
+            message = f'"{author}" успешно удален'
+            send_conclusion(message, True)
+        else:
+            message = f'Действие отменено'
+            send_conclusion(message, False)
+    else:
+        message = f'"{author}" нет в базе данных'
+        send_conclusion(message, False)
+
+
+print(db_get_poets())
