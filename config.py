070dc371fbca4484f66c69dbd3da43f339b6748f
Ryzhik Daniel <67552741+ZinixZay@users.noreply.github.com>
2022-05-01 15:13:45 +0300

    Перенес варианты ответа в файл конфига. Послание оставил в текстовом файле

--- config.py
+++ config.py
@@ -7,4 +7,9 @@ firebaseConfig = {
   'messagingSenderId': "654866140336",
   'appId': "1:654866140336:web:26a76e50b769703fa23f75",
   'measurementId': "G-DF86200KLD"
-};
+}
+stop_commands = ['выход', 'выключись']
+agreement_commands = ['да', 'конечно', 'давай']
+disagreement_commands = ['нет', 'не надо']
+readPoem_commands = ['читать', 'читать стих', 'читать стихи']
+learnPoem_commands = ['учить', 'учить стих', 'учить стихи']
